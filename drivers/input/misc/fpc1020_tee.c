0ad1b9fe019f0df62d96d85bb3695ab2415da112
Sultanxda <sultanxda@gmail.com>
2017-02-17 20:11:58 -0800

    misc: fpc1020: Increase fingerprintd nice value when screen is off
    The minimum nice value isn't needed to make fingerprint authentication/
    haptics consistent, so just go for a nice value of -1 instead.
    
    Change-Id: Ifd96bacd6b43dff671e11c2f089eebeea2adb807
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -569,7 +569,12 @@ static void fpc1020_suspend_resume(struct work_struct *work)
 		fpc1020_set_irq(fpc1020, true);
 		set_fingerprintd_nice(0);
 	} else {
-		set_fingerprintd_nice(MIN_NICE);
+		/*
+                 * Elevate fingerprintd priority when screen is off to ensure
+                 * the fingerprint sensor is responsive and that the haptic
+                 * response on successful verification always fires.
+                 */
+		set_fingerprintd_nice(-1);
 	}
 
 	sysfs_notify(&fpc1020->dev->kobj, NULL,
